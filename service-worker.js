a5b5779a50dcaaccf4c77d3b3c5ae46b8befc67a
nerik <erik@satellitestud.io>
2020-08-04 15:38:37 +0000

    Deploying to gh-pages from  @ fd2daace1ebb82ce9ed6966b9fac1bc342cd0eaf 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83601a8e13dc6638b3ecf34feebe2fcc.js"
+  "/whats-in-my-tile/build/precache-manifest.fb800e04f7731dfd3454f85131aa10c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/whats-in-my-tile/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
