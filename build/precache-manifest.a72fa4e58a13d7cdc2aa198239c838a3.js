fd4d6175e2eb66f90b07cad8ee82c6cb8bc0e1a7
erik <erik@satellitestud.io>
2020-08-04 17:32:55 +0200

    push build

---
+++ build/precache-manifest.a72fa4e58a13d7cdc2aa198239c838a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6344453f88f99b07f86f995fc99878",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef45d1996e6198db972",
+    "url": "/static/css/main.3450dd3a.chunk.css"
+  },
+  {
+    "revision": "959ac743024e6658c4e0",
+    "url": "/static/js/2.f31e6e22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f31e6e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef45d1996e6198db972",
+    "url": "/static/js/main.d367b849.chunk.js"
+  },
+  {
+    "revision": "51858e65aee23ba73c88",
+    "url": "/static/js/runtime-main.480331b0.js"
+  }
+]);
